bd78e3d0794d421479c31376412aeade38abf33b
Omar Mohammad <omarmo@cloudflare.com>
2025-01-21 20:32:54 +0000

    Add tag id and refactor

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,33 @@ import { html } from "hono/html";
 
 const app = new Hono();
 
-const regionDomainMap: Record<string, string> = {
-  us: "amazon.com",
-  uk: "amazon.co.uk",
-  in: "amazon.in",
-  ca: "amazon.ca",
-  de: "amazon.de",
-  nl: "amazon.nl",
-  es: "amazon.es",
-  sg: "amazon.sg",
+const countryData: Record<
+  string,
+  { domain: string; countryName: string; code: string }
+> = {
+  us: { domain: "amazon.com", countryName: "United States", code: "US" },
+  uk: { domain: "amazon.co.uk", countryName: "United Kingdom", code: "GB" },
+  in: { domain: "amazon.in", countryName: "India", code: "IN" },
+  ca: { domain: "amazon.ca", countryName: "Canada", code: "CA" },
+  de: { domain: "amazon.de", countryName: "Germany", code: "DE" },
+  nl: { domain: "amazon.nl", countryName: "Netherlands", code: "NL" },
+  es: { domain: "amazon.es", countryName: "Spain", code: "ES" },
+  sg: { domain: "amazon.sg", countryName: "Singapore", code: "SG" },
+  za: { domain: "amazon.co.za", countryName: "South Africe", code: "ZA" },
 };
 
-const cfCountryRegionMap: Record<string, string> = {
-  GB: "uk",
-  US: "us",
-  IN: "in",
-  CA: "ca",
-  DE: "de",
-  NL: "nl",
-  ES: "es",
-  SG: "sg",
-};
+// Default domain
+const defaultDomain = countryData["us"].domain;
 
-const countryNameMap: Record<string, string> = {
-  us: "United States",
-  uk: "United Kingdom",
-  in: "India",
-  ca: "Canada",
-  de: "Germany",
-  nl: "Netherlands",
-  es: "Spain",
-  sg: "Singapore",
-};
+// Affiliate ID
+const tagId = "omarmosid-21";
 
-const defaultDomain = regionDomainMap["us"];
+const getCountryData = (countryCode: unknown) => {
+  const country = Object.values(countryData).find(
+    (country) => country.code === countryCode
+  );
+  return country;
+};
 
 const extractAmazonProductId = (url?: string) => {
   if (!url) return;
@@ -46,10 +39,13 @@ const extractAmazonProductId = (url?: string) => {
   return match ? match[1] : null;
 };
 
-app.get("/", (c) => {
-  const originCountry = c.req.raw.cf?.country;
-  const originRegion = cfCountryRegionMap[originCountry as string] || "us";
+const getAmazonUrl = (domain: string, productId: string) => {
+  return `https://${domain}/dp/${productId}?tag=${tagId}`;
+};
 
+// Usage of the consolidated object
+app.get("/", (c) => {
+  const originCountryCode = c.req.raw.cf?.country;
   return c.html(
     html` <!DOCTYPE html>
       <html lang="en">
@@ -110,13 +106,12 @@ app.get("/", (c) => {
                   class="w-full px-4 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
                 >
                   <option value="">
-                    Use my location - ${countryNameMap[originRegion]}
+                    Use my location -
+                    ${getCountryData(originCountryCode)?.countryName}
                   </option>
-                  ${Object.entries(regionDomainMap).map(
-                    ([region, domain]) =>
-                      html`<option value="${region}">
-                        ${countryNameMap[region]}
-                      </option>`
+                  ${Object.entries(countryData).map(
+                    ([region, { countryName }]) =>
+                      html`<option value="${region}">${countryName}</option>`
                   )}
                 </select>
               </div>
@@ -147,10 +142,11 @@ app.get("/r", (c) => {
     return c.json({ msg: "Error. Invalid Amazon product URL" });
   }
 
-  const region = selectedCountry || cfCountryRegionMap[originCountry as string];
-  const domain = regionDomainMap[region] || defaultDomain;
+  const region = selectedCountry || originCountry;
+  const domain = getCountryData(region as string)?.domain || defaultDomain;
+  const redirectUrl = getAmazonUrl(domain, productId);
 
-  return c.redirect(`https://${domain}/dp/${productId}`);
+  return c.redirect(redirectUrl);
 });
 
 app.get("/r/:id", (c) => {
@@ -163,10 +159,11 @@ app.get("/r/:id", (c) => {
     return c.json({ msg: "Error. Invalid Amazon product URL" });
   }
 
-  const region = selectedCountry || cfCountryRegionMap[originCountry as string];
-  const domain = regionDomainMap[region] || defaultDomain;
+  const region = selectedCountry || originCountry;
+  const domain = getCountryData(region as string)?.domain || defaultDomain;
+  const redirectUrl = getAmazonUrl(domain, productId);
 
-  return c.redirect(`https://${domain}/dp/${productId}`);
+  return c.redirect(redirectUrl);
 });
 
 export default app;
